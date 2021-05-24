bbaef25e5bcb915359200c739e228234c329cae7
Trehan Sangpriya <trehansangpriya@gmail.com>
2021-05-24 15:08:45 +0530

    feat : Nav, Meta, Hero Components + Icons & PWA

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,19 @@
 import React from "react"
-import Button from "../components/Utilities/Button";
+import HeroSection from "../components/Utilities/HeroSection";
 import { WebProvider } from '../contexts/WebConext'
 import Layout from './../components/Layout/Layout';
+import heroSectionData from '../data/Home/heroSection.json'
 
 export default function Home() {
 
   return (
     <WebProvider>
       <Layout>
-        <div className="section">
-
-        </div>
+        <HeroSection
+          content={heroSectionData.content}
+          title={heroSectionData.title}
+          img={heroSectionData.img}
+          link={heroSectionData.link} />
       </Layout>
     </WebProvider>
   )
