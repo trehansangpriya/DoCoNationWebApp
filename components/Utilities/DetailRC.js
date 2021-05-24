6bc5cefd702dac0065ac5f8ef26bd5898e9908b9
Saketkhare2000 <73240636+Saketkhare2000@users.noreply.github.com>
2021-05-25 01:07:46 +0530

    feat : detailRC component

--- components/Utilities/DetailRC.js
+++ components/Utilities/DetailRC.js
@@ -1,11 +1,65 @@
-import React from 'react'
+import React from 'react';
+import detailRCstyles from '../../styles/Utilities/Detail.module.css';
+import Button from './Button';
 
-const DetailRC = () => {
-    return (
-        <div>
-
+const DetailRC = ({
+  generalTitle,
+  coloredTitle,
+  content,
+  img,
+  btnPri,
+  btnSec,
+  rev,
+  alt,
+}) => {
+  return rev ? (
+    <div className={'d-flex d-row-rev ' + detailRCstyles.detailRC}>
+      <div className={detailRCstyles.imgContainer}>
+        <img src={img} alt={alt} />
+      </div>
+      <div className={detailRCstyles.textDetail}>
+        <h2>
+          {generalTitle} <span className="c-blue">{coloredTitle}</span>{' '}
+        </h2>
+        <p>{content}</p>
+        <div className={'btns '}>
+          {btnPri && (
+            <Button
+              className={detailRCstyles.randomClass}
+              label={btnPri.label}
+              type={btnPri.type + ' ' + detailRCstyles.randomClass}
+            />
+          )}
+          {btnSec && <Button label={btnSec.label} type={btnSec.type} />}
         </div>
-    )
-}
+      </div>
+    </div>
+  ) : (
+    <div className={'d-flex ' + detailRCstyles.detailRC}>
+      <div className={detailRCstyles.imgContainer}>
+        <img src={img} alt={alt} />
+      </div>
+      <div className={detailRCstyles.textDetail}>
+        <h2>
+          {generalTitle} <span className="c-blue">{coloredTitle}</span>{' '}
+        </h2>
+        <p>{content}</p>
+        <div className={'btns ' + detailRCstyles.buttonRes}>
+          {btnPri && (
+            <Button
+              className={detailRCstyles.randomClass}
+              label={btnPri.label}
+              type={btnPri.type + ' ' + detailRCstyles.mRight}
+              link={'#'}
+            />
+          )}
+          {btnSec && (
+            <Button label={btnSec.label} type={btnSec.type} link={'#'} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default DetailRC
+export default DetailRC;
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,35 @@
-import React from "react"
-import Button from "../components/Utilities/Button";
-import { WebProvider } from '../contexts/WebConext'
+import React from 'react';
+import Button from '../components/Utilities/Button';
+import { WebProvider } from '../contexts/WebConext';
 import Layout from './../components/Layout/Layout';
-
+import dataRC from '../data/Home/detailRC.json';
+import DetailRC from '../components/Utilities/DetailRC';
 export default function Home() {
-
   return (
     <WebProvider>
       <Layout>
         <div className="section">
-
+          <DetailRC
+            generalTitle={dataRC[0].title.general}
+            coloredTitle={dataRC[0].title.highlight}
+            content={dataRC[0].content}
+            img={dataRC[0].img}
+            btnPri={dataRC[0].btns.primary}
+            btnSec={dataRC[0].btns.secondary}
+            rev={dataRC[0].rev}
+            alt={dataRC[0].alt}
+          />
+          <DetailRC
+            generalTitle={dataRC[1].title.general}
+            coloredTitle={dataRC[1].title.highlight}
+            content={dataRC[1].content}
+            img={dataRC[1].img}
+            btnPri={dataRC[1].btns.primary}
+            rev={dataRC[1].rev}
+            alt={dataRC[1].alt}
+          />
         </div>
       </Layout>
     </WebProvider>
-  )
+  );
 }
