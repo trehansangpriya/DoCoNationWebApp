844dd2f9afddecea11eacf2319081d26853121c5
Neeha3 <Neeha3@users.noreply.github.com>
2021-05-24 23:29:35 +0530

    feat : feature cards

--- components/Utilities/FeatureCards/FeatureCard.js
+++ components/Utilities/FeatureCards/FeatureCard.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import featureCardStyles from '../../../styles/Utilities/FeatureCards/FeatureCard.module.css'
 
-const FeatureCard = () => {
+const FeatureCard = ({ data }) => {
+
+
+
     return (
-        <div>
+
+        <div className={featureCardStyles.featContainer}>
+            {data.map(feature => (
+                <div className={featureCardStyles.feature + ' d-flex'}>
+
+                    <div className={featureCardStyles.icon}>
+                        <img src={feature.icon} alt="Something" />
+                    </div>
+
+                    <div className={featureCardStyles.title}>
+                        {feature.title}
+                    </div>
+
+                </div>
+            ))}
 
         </div>
+
     )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import React from "react"
 import Button from "../components/Utilities/Button";
+import FeatureCards from "../components/Utilities/FeatureCards";
 import { WebProvider } from '../contexts/WebConext'
 import Layout from './../components/Layout/Layout';
+import featureCardData from '../data/Home/features.json'
 
 export default function Home() {
 
@@ -11,6 +13,7 @@ export default function Home() {
         <div className="section">
 
         </div>
+        <FeatureCards title="Why Be A Part Of It?" data={featureCardData} />
       </Layout>
     </WebProvider>
   )
