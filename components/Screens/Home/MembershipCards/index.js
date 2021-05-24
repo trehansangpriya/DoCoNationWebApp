1779abfa37a43bbd91cf7ae0c226de047b25e4cd
Trehan Sangpriya <trehansangpriya@gmail.com>
2021-05-24 23:32:27 +0530

    feat : Membership Area, About Component

--- components/Screens/Home/MembershipCards/index.js
+++ components/Screens/Home/MembershipCards/index.js
@@ -1,12 +1,17 @@
-import React from "react";
-import MembershipCard from "./MembershipCard";
-import styles from "../../../../styles/screens/home.module.css";
+import React from 'react';
+import MembershipCard from './MembershipCard';
+import styles from '../../../../styles/Screens/Home/MembershipCards.module.css';
 
 const MembershipCards = ({ data }) => {
   return (
     <div className={styles.memberContainer}>
       {data.map((data) => (
-        <MembershipCard icon={data.icon_url} title={data.title} />
+        <MembershipCard
+          icon={data.icon_url}
+          title={data.title}
+          alt={data.alt}
+          key={data.id}
+        />
       ))}
     </div>
   );
